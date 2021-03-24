f7300f48afbb99cdd693d2ffac1b920784add309
ngbla <ngbla@server.centos.local>
2021-03-24 15:14:09 +0000

    Init

---
+++ index.py
@@ -0,0 +1,39 @@
+import cv2
+
+# Load the cascade
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# To capture video from webcam. 
+cap = cv2.VideoCapture(0)
+# To use a video file as input 
+# cap = cv2.VideoCapture('filename.mp4')
+
+while True:
+    # Read the frame
+    _, img = cap.read()
+    # Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Detect the faces
+    #faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    # Detect faces in the image
+    faces = face_cascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags = cv2.cv.CV_HAAR_SCALE_IMAGE
+    )
+
+    print "Found {0} faces!".format(len(faces))
+    # Draw the rectangle around each face
+    for (x, y, w, h) in faces:
+        #cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    # Display
+    cv2.imshow('img', img)
+    # Stop if escape key is pressed
+    k = cv2.waitKey(30) & 0xff
+    if k==27:
+        break
+# Release the VideoCapture object
+cap.release()
---
+++ test.py
@@ -0,0 +1,52 @@
+import numpy as np
+import cv2 as cv
+#import matplotlib
+
+#face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv.CascadeClassifier('haarcascade_eye.xml')
+
+cap = cv.VideoCapture(0)
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    # if frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+
+
+
+    # Our operations on the frame come here
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    # Display the resulting frame
+    #cv.imshow('frame', gray)
+
+
+    # Detect the faces
+    #faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    # Draw the rectangle around each face   
+    for (x,y,w,h) in faces:
+        frame = cv.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex,ey,ew,eh) in eyes:
+            cv.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+
+    # Display
+    cv.imshow('img', frame)
+    # Stop if escape key is pressed
+    #k = cv.waitKey(30) & 0xff
+
+
+    if cv.waitKey(1) == ord('q'):
+        break
+# When everything done, release the capture
+cap.release()
+cv.destroyAllWindows()
