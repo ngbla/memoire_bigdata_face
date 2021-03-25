06fc32ccaa643b576bcf7dd3dc54dbb6daf44e01
ngbla <ngbla@server.centos.local>
2021-03-25 14:56:38 +0000

    Projet

--- demo.py
+++ demo.py
@@ -2,7 +2,7 @@
 import numpy as np
 import cv2 as cv
 #import matplotlib
-
+print(cv.__version__)
 #face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
 face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv.CascadeClassifier('haarcascade_eye.xml')
