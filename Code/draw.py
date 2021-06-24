dd9cf3d198f3921cd7fc183902732fd877473edf
ngbla <ngbla.elvis@hotmail.fr>
2021-06-24 07:15:33 -0700

    nouveau fichier : Note.txt 	nouveau fichier : draw.py 	modifié :         haar_detection.py 	renommé :         test/1.jpg -> img_test/1.jpg 	renommé :         dataset/1/1_1.jpg -> img_training/1/1_1.jpg 	renommé :         dataset/1/1_10.jpg -> img_training/1/1_10.jpg 	renommé :         dataset/1/1_11.jpg -> img_training/1/1_11.jpg 	renommé :         dataset/1/1_12.jpg -> img_training/1/1_12.jpg 	renommé :         dataset/1/1_13.jpg -> img_training/1/1_13.jpg 	renommé :         dataset/1/14.jpg -> img_training/1/1_14.jpg 	renommé :         dataset/1/15.jpg -> img_training/1/1_15.jpg 	renommé :         dataset/1/1_2.jpg -> img_training/1/1_2.jpg 	renommé :         dataset/1/1_3.jpg -> img_training/1/1_3.jpg 	renommé :         dataset/1/1_4.jpg -> img_training/1/1_4.jpg 	renommé :         dataset/1/1_5.jpg -> img_training/1/1_5.jpg 	renommé :         dataset/1/1_6.jpg -> img_training/1/1_6.jpg 	renommé :         dataset/1/1_7.jpg -> img_training/1/1_7.jpg 	renommé :         dataset/1/1_8.jpg -> img_training/1/1_8.jpg 	renommé :         dataset/1/1_9.jpg -> img_training/1/1_9.jpg 	nouveau fichier : predict.py 	nouveau fichier : reconnaissance.py 	nouveau fichier : train_model.py 	nouveau fichier : training.yml

---
+++ Code/draw.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 23 10:23:12 2021
+
+@author: ngbla
+"""
+
+#function to draw rectangle on image 
+#according to given (x, y) coordinates and 
+#given width and heigh
+def draw_rectangle(img, rect):
+    (x, y, w, h) = rect
+    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    
+#function to draw text on give image starting from
+#passed (x, y) coordinates. 
+def draw_text(img, text, x, y):
+    cv2.putText(img, text, (x, y), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 255, 0), 2)
---
+++ Code/predict.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 23 10:18:43 2021
+@author: ngbla
+"""
+
+#import OpenCV module
+import cv2
+#import os module for reading training data directories and paths
+import os
+#import numpy to convert python lists to numpy arrays as 
+#it is needed by OpenCV face recognizers
+import numpy as np
+
+#this function recognizes the person in image passed
+#and draws a rectangle around detected face with name of the 
+#subject
+def predict(test_img):
+    #make a copy of the image as we don't want to chang original image
+    img = test_img.copy()
+    #detect face from the image
+    face, rect = detect_face(img)
+
+    #predict the image using our face recognizer 
+    label, confidence = face_recognizer.predict(face)
+    #get name of respective label returned by face recognizer
+    label_text = subjects[label]
+    
+    #draw a rectangle around face detected
+    draw_rectangle(img, rect)
+    #draw name of predicted person
+    draw_text(img, label_text, rect[0], rect[1]-5)
+    
+    return img
